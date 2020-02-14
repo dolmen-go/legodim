c4a4fff83fcb8876d4194dd8d19cfdd5cb2998b4
Olivier Mengué <dolmen@cpan.org>
2020-02-14 23:36:50 +0100

    cmd/toypad-read: implement decoding of character ids

--- cmd/toypad-read/main.go
+++ cmd/toypad-read/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"log"
 
-	"github.com/dolmen-go/legodim/toypad"
 	"github.com/karalabe/hid"
+
+	"github.com/dolmen-go/legodim/tag"
+	"github.com/dolmen-go/legodim/toypad"
 )
 
 func main() {
@@ -32,10 +34,33 @@ func main() {
 			log.Printf("Event: %v\n", ev)
 			if ev.Action == toypad.Add {
 				count[int(ev.Pad-1)]++
-				cb := func(uint8, []byte, error) {}
-				tp.Send(toypad.TagRead(ev.Index, 0x23, cb))
-				tp.Send(toypad.TagRead(ev.Index, 0x27, cb))
-				tp.Send(toypad.TagRead(ev.Index, 0x2B, cb))
+
+				//tp.Send(toypad.SetColor(ev.Pad, toypad.RGB{255, 0, 0}))
+				tp.Send(toypad.SetColor(ev.Pad, toypad.RGB{77, 197, 172}))
+
+				uid := tag.UID(ev.UID)
+				key := uid.Key()
+
+				/*
+					tp.Send(toypad.TagRead(ev.Index, 0x23, cb))
+					tp.Send(toypad.TagRead(ev.Index, 0x27, cb))
+					tp.Send(toypad.TagRead(ev.Index, 0x2B, cb))
+				*/
+				for p := 7; p < 40; p += 4 {
+					p := p
+					cb := func(status uint8, b []byte, err error) {
+						if len(b) < 4 {
+							log.Println(err)
+							return
+						}
+						log.Println("Reading page", p)
+						if p <= 36 && p+4 >= 37 {
+							c := key.DecryptCharacter(b[(36-p)*4:])
+							log.Printf("\033[1;33mCharacter: %d\033[m", c)
+						}
+					}
+					tp.Send(toypad.TagRead(ev.Index, uint8(p), cb))
+				}
 
 				// tp.Send(toypad.SetColor(ev.Pad, toypad.RGB{ev.UID[0], ev.UID[1], ev.UID[2]}))
 				tp.Send(toypad.Flash(ev.Pad, 5, 10, 50, toypad.RGB{ev.UID[4], ev.UID[5], ev.UID[6]}))
